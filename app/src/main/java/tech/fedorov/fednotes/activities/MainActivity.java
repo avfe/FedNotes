ef09a72a4866946d7eb01938ed1d4d37e1369817
avfe <sen4ka@mail.ru>
2021-06-10 07:30:38 +0300

    Note Activity Layout was added

--- app/src/main/java/tech/fedorov/fednotes/activities/MainActivity.java
+++ app/src/main/java/tech/fedorov/fednotes/activities/MainActivity.java
@@ -2,20 +2,28 @@ package tech.fedorov.fednotes.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import tech.fedorov.fednotes.R;
+import tech.fedorov.fednotes.activities.NoteActivity;
 
 public class MainActivity extends AppCompatActivity {
     private EditText searchField;
+    private ImageView addButton;
+
+    public static final int CODE_ADD_NOTE = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         searchField = findViewById(R.id.search_field);
+        addButton = findViewById(R.id.add_button);
 
         searchField.setOnFocusChangeListener(new View.OnFocusChangeListener() {
             @Override
@@ -28,5 +36,12 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent noteIntent = new Intent(getApplicationContext(), NoteActivity.class);
+                startActivityForResult(noteIntent, CODE_ADD_NOTE);
+            }
+        });
     }
 }
--- app/src/main/java/tech/fedorov/fednotes/activities/NoteActivity.java
+++ app/src/main/java/tech/fedorov/fednotes/activities/NoteActivity.java
@@ -1,13 +1,43 @@
 package tech.fedorov.fednotes.activities;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import tech.fedorov.fednotes.R;
+
 public class NoteActivity extends AppCompatActivity {
+    private ImageButton backButton;
+    private ImageButton doneButton;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_note);
+
+        backButton = findViewById(R.id.back_button);
+        doneButton = findViewById(R.id.done_button);
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+
+        doneButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                save();
+                finish();
+            }
+        });
+
     }
 
+    private void save() {
+
+    }
 }
